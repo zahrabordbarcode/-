512c173daaf7a10978239fecdc0bf1fc789114c2
zahrabordbarcode <nilbor8802@gmail.com>
2024-06-21 10:58:00 -0700

    Add files via upload

---
+++ تمرین آخر ساختمان داده.py
@@ -0,0 +1,31 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[i] < arr[left]:
+        largest = left
+
+    if right < n and arr[largest] < arr[right]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        heapify(arr, i, 0)
+
+# ورودی‌ها را دریافت کنید
+data = list(map(int, input("لطفا اعداد را وارد کنید: ").split()))
+
+heap_sort(data)
+print("آرایه مرتب شده:")
+print(data)
